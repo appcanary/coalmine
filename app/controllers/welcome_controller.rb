505bcd4175a83ee10f0e01f30aa6901542e91290
Phill MV <phillmv@okayfail.com>
2015-01-24 19:15:46 -0500

    Decided against using mailchimp, for now.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def beta_list
+    BetaUser.create email: params[:email]
+    flash[:subscribed] = "Yay! We'll give you a shout once we're ready."
+
+    redirect_to root_path
+  end
 end
