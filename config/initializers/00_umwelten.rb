b79e04fd5c143f9f6cefb26fce0e9f4bb87d02cd
Phill MV <phillmv@okayfail.com>
2015-02-20 16:05:41 -0500

    Reorged Umwelten to handle secrets, added canary_api opts.

--- config/initializers/00_umwelten.rb
+++ config/initializers/00_umwelten.rb
@@ -1,6 +1,11 @@
-settings_path = File.join(Rails.root, "config/settings", "*")
+settings_path = File.join(Rails.root, "config/settings", "**/*.yml")
 Dir[settings_path].each do |file|
   new_conf = YAML.load_file(file)
+  
+  if new_conf[Rails.env]
+    new_conf = new_conf[Rails.env]
+  end
+
   key = new_conf.keys.first
   if key
     Rails.configuration.send("#{key}=", OpenStruct.new(new_conf[key]))
