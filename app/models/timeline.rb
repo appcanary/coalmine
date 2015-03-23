805ea21ad7ae2566c68a42f8dbfd43fec4fc10aa
Max Veytsman <maxim@ontoillogical.com>
2015-03-23 16:20:43 -0400

    Demo timeline with a server & two apps

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -1,6 +1,25 @@
 class Timeline
   def self.for(user)
+    server = Server.new(:name => "droplet37a")
     arr = [Event.new(:kind => :new_server,
-                     :server => Server.new(:name => "demo-server-53"))]
+                     :created_at => Time.now,
+                     :server => server),
+
+           Event.new(:kind => :new_app,
+                     :created_at => Time.now,
+                     :app => App.new(:name => "Airbnb for toothbrushes",
+                                     :platforms => "Ruby",
+                                     :last_synced_at => 2.seconds.from_now,
+                                     :server => server)
+                    ),
+           Event.new(:kind => :new_app,
+                     :created_at => Time.now,
+                     :app => App.new(:name => "Uber for dogs",
+                                     :platforms => "Node",
+                                     :last_synced_at => 2.seconds.from_now,
+                                     :server => server)
+                    )
+          ]
+    arr.reverse
   end
 end
