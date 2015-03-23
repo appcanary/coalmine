3fa68d094382cc3849e300b573c37c2df793d48e
Phill MV <phillmv@okayfail.com>
2015-03-23 17:21:41 -0400

    Added demo seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.where(:email => 'sama@ycombinator.com').first_or_create(
+  :password => "500 startups",
+  :password_confirmation => "500 startups")
