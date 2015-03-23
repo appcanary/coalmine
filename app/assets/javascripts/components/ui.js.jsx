e7773792e37acb25f41a3939eb29db3fe637ef23
Phill MV <phillmv@okayfail.com>
2015-03-23 19:09:39 -0400

    Added emberui primary styles.

--- app/assets/javascripts/components/ui.js.jsx
+++ app/assets/javascripts/components/ui.js.jsx
@@ -1,7 +1,8 @@
 var Eui_button = React.createClass({
   render: function() {
+    var btn_type = this.props.style || "default"
     return (
-      <eui-button className="ember-view eui-button-medium-default">
+      <eui-button className={"ember-view eui-button-medium-" + btn_type}>
         <button aria-label={this.props.value}></button>
         <div className="eui-component">
           <div className="eui-component-wrapper">
