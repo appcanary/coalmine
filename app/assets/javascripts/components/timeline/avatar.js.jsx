d18185b0e59c6ab10927f5530589a5ff454122a5
Max Veytsman <maxim@ontoillogical.com>
2015-03-23 23:05:54 -0400

    Avatar link widget handles cases without links sllllliiiiiiiiiiiighhhtly better

--- app/assets/javascripts/components/timeline/avatar.js.jsx
+++ app/assets/javascripts/components/timeline/avatar.js.jsx
@@ -1,10 +1,11 @@
 var AvatarWidget = React.createClass({
   render: function() {
     var size = this.props.size || ""
+    var handler = this.props.onClicked || function(e) {e.preventDefault()}
     return (
       <span>
         <img src={"data:image.png;base64," + this.props.model.avatar} className={"icon " + size} onClick={this.props.onClicked}/>
-        <strong>&nbsp;<a href="#" onClick={this.props.onClicked}>{this.props.model.name}</a></strong>
+        <strong>&nbsp;<a href= "#" onClick={handler}>{this.props.model.name}</a></strong>
       </span>
 
     );
