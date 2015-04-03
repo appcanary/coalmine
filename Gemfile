650c4a4cc4c071b2178a0ebc4f198581239dbe96
Phill MV <phillmv@okayfail.com>
2015-04-03 16:18:24 -0400

    Disabled liverreload for now.

--- Gemfile
+++ Gemfile
@@ -74,7 +74,7 @@ end
 
 
 group :development do
-  gem "rack-livereload"
-  gem 'guard-livereload', '~> 2.4', require: false
+  # gem "rack-livereload"
+  # gem 'guard-livereload', '~> 2.4', require: false
   gem 'guard-minitest'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
  
-  config.middleware.use Rack::LiveReload
+  # config.middleware.use Rack::LiveReload
   config.react.variant = :development
   
 end
