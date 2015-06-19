65c3d9926a1ba0c262ab52491ef66cb726a1a658
Phill MV <phillmv@okayfail.com>
2015-06-18 19:16:19 -0700

    Interim cleanup; attributes are now more flexible per #54

--- app/models/concerns/track_attributes.rb
+++ app/models/concerns/track_attributes.rb
@@ -2,46 +2,84 @@ module TrackAttributes
   extend ActiveSupport::Concern
 
   included do
-    # define_singleton_method(:attr_reader) do |*params|
-    #   @attr_readers ||= []
-    #   @attr_readers.concat params
-    #   super(*params)
-    # end
-
-    # define_singleton_method(:attr_writer) do |*params|
-    #   @attr_writers ||= []
-    #   @attr_writers.concat params
-    #   super(*params)
-    # end
-
     define_singleton_method(:attr_params) do |*params|
-      @attr_accessors ||= []
-      @attr_accessors.concat params
+      @attr_params ||= []
+      @attr_params.concat params
       attr_accessor(*params)
     end
+  end
 
-    define_singleton_method(:map_key) do |hsh|
-      @map_key ||= {}
-      @map_key = @map_key.merge(hsh)
+  class_methods do
+    def has_many(klass, key = nil)
+      @has_many ||= {}
+
+      if key.nil?
+        key = klass.model_name.route_key
+      end
+
+      # ensure we got strs and not syms
+      key = key.to_s
+
+      @has_many = @has_many.merge({key => klass})
     end
+
+    def parse(attr, canary = nil)
+      if canary
+        self.client = canary
+      end
+
+      if attr.is_a? Array
+        attr.map { |params| self.new(params) }
+      else
+        self.new(attr)
+      end
+    end
+
+    def client=(canary)
+      @canary = canary
+    end
+
+    def client
+      @canary || Canary.new(nil)
+    end
+
   end
 
-  def attr_readers
-    self.class.instance_variable_get('@attr_readers')
+
+  # TODO remember to add parent association?
+  # TODO: handle collections
+  # for now this is distinct from the Canary#wrap code in so far
+  # that nested collections aren't
+  # expected to have cursors?
+
+  def initialize(params = {})
+    assoc = self.associations
+    attrs = self.attr_params.map(&:to_s)
+    params.each do |attr, value|
+      setter = attr.to_s.tr("-", "_")
+      if klass = assoc[setter]
+        self.public_send("#{setter}=", klass.parse(value))
+      elsif attrs.include?(setter)
+        self.public_send("#{setter}=", value)
+      end
+    end if params
   end
 
-  def attr_writers
-    self.class.instance_variable_get('@attr_writers')
+  def attributes
+    Hash[attr_params.map { |k| [k, send(k)] }]
   end
 
-  def attr_accessors
-    self.class.instance_variable_get('@attr_accessors') || []
+  def associations
+    self.class.instance_variable_get('@has_many') || {}
   end
 
-  def map_key
-    self.class.instance_variable_get('@map_key') || {}
+
+  def attr_params
+    self.class.instance_variable_get('@attr_params') || []
   end
 
-  # TODO: convert to AR style attributes
+  def canary
+    self.class.client
+  end
 
 end
