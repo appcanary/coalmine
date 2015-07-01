d8eb8f1658c51a22ff78f8fb6d5491c75caa0e10
Phill MV <phillmv@okayfail.com>
2015-07-01 12:48:23 -0700

    CanaryBase: improved has_many, added enforce_collection, #93 and #53"

--- app/models/concerns/param_attributes.rb
+++ app/models/concerns/param_attributes.rb
@@ -7,9 +7,15 @@ module ParamAttributes
       @attr_params.concat params
       attr_accessor(*params)
     end
+
+    define_singleton_method(:attr_enforce_collection) do |*params|
+      @attr_enforce_collection ||= []
+      @attr_enforce_collection.concat params
+    end
   end
 
   class_methods do
+    # has many ALWAYS assumes a collection
     def has_many(klass, key = nil)
       @has_many ||= {}
 
@@ -33,13 +39,29 @@ module ParamAttributes
         self.client = canary
       end
 
+      # if this is not a has_many
+      # attrs being an array is technically
+      # an error
       if attr.is_a? Array
         attr.map { |params| self.new(params) }
+      elsif attr.nil?
+        return nil
       else
         self.new(attr)
       end
     end
 
+    # enforce that has_many always is
+    # an array.
+    def parse_many(attr, canary = nil)
+      if canary
+        self.client = canary
+      end
+
+      all_attrs = wrap_array(attr)
+      all_attrs.map { |params| self.new(params) }
+    end
+      
     def client=(canary)
       @canary = canary
     end
@@ -52,22 +74,41 @@ module ParamAttributes
       @attr_params || []
     end
 
+    def wrap_array(attr)
+      if attr.nil?
+        return []
+      elsif !attr.is_a? Array
+        return [attr]
+      else
+        return attr
+      end
+    end
+
   end
 
 
   # TODO remember to add parent association
  
-  def initialize(params = {})
-    assoc = self.associations
+  def initialize(input_params = {})
+    coll_association = self.has_many_associations
     attrs = self.attr_params.map(&:to_s)
-    params.each do |attr, value|
-      setter = attr.to_s.tr("-", "_")
-      if klass = assoc[setter]
-        self.public_send("#{setter}=", klass.parse(value))
-      elsif attrs.include?(setter)
-        self.public_send("#{setter}=", value)
+    must_be_coll = attr_enforce_collection_params.map(&:to_s)
+
+    params = sanitize_param_keys(input_params)
+    attrs.each do |setter|
+      value = params[setter]
+      if klass = coll_association[setter]
+        self.public_send("#{setter}=", klass.parse_many(value))
+      else
+
+        if must_be_coll.include?(setter)
+          self.public_send("#{setter}=", wrap_array(value))
+        else
+          self.public_send("#{setter}=", value)
+        end
+
       end
-    end if params
+    end
   end
 
   def attributes
@@ -75,9 +116,14 @@ module ParamAttributes
   end
 
 
-  def associations
+  def has_many_associations
     self.class.instance_variable_get('@has_many') || {}
   end
+
+  def attr_enforce_collection_params
+    self.class.instance_variable_get('@attr_enforce_collection') || []
+  end
+
 
 
   def attr_params
@@ -86,8 +132,21 @@ module ParamAttributes
 
   protected
 
+  def sanitize_param_keys(params = {})
+    new_params = {}
+    params.each_pair do |k, v|
+      key = k.to_s.tr("-", "_")
+      new_params[key] = v
+    end
+    new_params
+  end
+ 
   def canary
     self.class.client
+  end
+
+  def wrap_array(args)
+    self.class.wrap_array(args)
   end
 
 end
--- test/models/canary_base_test.rb
+++ test/models/canary_base_test.rb
@@ -54,7 +54,30 @@ class CanaryBaseTest < ActiveSupport::TestCase
       assert_equal "test1", obj.attributes[:heartbeat_at]
     end
 
-    it "should handle associations" do
+    it "should handle enforce_collection" do
+      klass = Class.new(CanaryBase) do
+        attr_params :heartbeat_at, :many_things
+        attr_enforce_collection :many_things
+      end
+
+      hsh = { "heartbeat-at" => "test1", "many-things" => "foo" }
+
+      obj = klass.new(hsh)
+      assert obj.heartbeat_at
+      assert_equal 1, obj.many_things.size
+      assert_equal "foo", obj.many_things[0]
+
+      hsh_missing = { }
+
+      obj_missing = klass.new(hsh_missing)
+      assert_equal false, obj_missing.many_things.nil?
+      assert_equal 0, obj_missing.many_things.size
+    end
+  end
+
+  describe "CanaryBase associations" do
+
+    it "has many associations always return arrays" do
       test_association_class = Class.new(CanaryBase) do
         attr_params :sub_param1
         def self.name
@@ -72,11 +95,13 @@ class CanaryBaseTest < ActiveSupport::TestCase
 
       obj = klass.new(hsh)
       assert obj.param1
-      assert obj.many_objects
-      assert_equal "test2", obj.many_objects.sub_param1
+      assert obj.many_objects.present?
+      assert_equal 1, obj.many_objects.size
+      assert_equal test_association_class, obj.many_objects.first.class
+      assert_equal "test2", obj.many_objects.first.sub_param1
     end
 
-    it "should handle collections of associations" do
+    it "has many associations should handle actual collections" do
       test_association_class = Class.new(CanaryBase) do
         attr_params :sub_param1
         def self.name
@@ -98,9 +123,57 @@ class CanaryBaseTest < ActiveSupport::TestCase
       obj = klass.new(hsh)
       assert obj.param1
       assert_equal obj.many_objects.size, 2
+      assert_equal test_association_class, obj.many_objects[0].class
       assert_equal "test2", obj.many_objects[0].sub_param1
       assert_equal "test3", obj.many_objects[1].sub_param1
     end
+
+    it "when an association collection is empty, it should be []" do
+      test_association_class = Class.new(CanaryBase) do
+        attr_params :sub_param1
+        def self.name
+          "many_object"
+        end
+      end
+
+      klass = Class.new(CanaryBase) do
+        attr_params :param1, :many_objects
+        has_many test_association_class
+      end
+
+
+      hsh_arr = { "param1" => "test1", 
+              "many_objects" => [] 
+      }
+
+      obj_arr = klass.new(hsh_arr)
+      assert obj_arr.param1
+      assert_equal true, obj_arr.many_objects.empty?
+      assert_not_equal nil, obj_arr.many_objects
+      assert_equal 0, obj_arr.many_objects.size
+
+
+      hsh_nil = { "param1" => "test1", 
+              "many_objects" => nil 
+      }
+
+      obj_nil = klass.new(hsh_nil)
+      assert obj_nil.param1
+      assert_equal true, obj_nil.many_objects.empty?
+      assert_not_equal nil, obj_nil.many_objects
+      assert_equal 0, obj_nil.many_objects.size
+
+
+      hsh_missing = { "param1" => "test1",}
+
+      obj_missing = klass.new(hsh_missing)
+      assert obj_missing.param1
+      assert_equal true, obj_missing.many_objects.empty?
+      assert_not_equal nil, obj_missing.many_objects
+      assert_equal 0, obj_missing.many_objects.size
+
+    end
+
   end
 
   describe "CanaryBase.parse" do
