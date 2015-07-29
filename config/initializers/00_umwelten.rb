5a37bc80a1593163d6acf22b3a845677905a4885
Phill MV <phillmv@okayfail.com>
2015-07-28 23:59:29 -0700

    Changed how umwelten works so it respects symlinks

--- config/initializers/00_umwelten.rb
+++ config/initializers/00_umwelten.rb
@@ -1,7 +1,32 @@
-settings_path = File.join(Rails.root, "config/settings", "**/**/*.yml")
+require 'find'
 
-Dir[settings_path].each do |file|
+settings_path = File.join(Rails.root, "config/settings")
+
+search_dirs = [settings_path]
+files = []
+
+until search_dirs.empty?
+  search_path = search_dirs.shift 
+ 
+  Find.find(search_path) do |path|
+  
+    unless path == search_path 
+    
+      if File.directory?( path )
+        puts "sd2 #{search_dirs}"
+        search_dirs << path
+      else
+        files << path
+      end
+
+    end
+  end
+end
+
+
+files.each do |file|
   new_conf = YAML.load_file(file)
+  file_name = File.basename(file)
   next unless new_conf.is_a? Hash
 
   if new_conf[Rails.env]
