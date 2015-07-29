6f893d19d75e1482ceccd51e3116c24b50913b30
Phill MV <phillmv@okayfail.com>
2015-07-28 23:14:04 -0700

    Improved umwelten initializer to handle subdirectories

--- config/initializers/00_umwelten.rb
+++ config/initializers/00_umwelten.rb
@@ -1,13 +1,17 @@
-settings_path = File.join(Rails.root, "config/settings", "**/*.yml")
+settings_path = File.join(Rails.root, "config/settings", "**/**/*.yml")
+
 Dir[settings_path].each do |file|
   new_conf = YAML.load_file(file)
+  next unless new_conf.is_a? Hash
 
   if new_conf[Rails.env]
-    new_conf = new_conf[Rails.env]
+    new_settings = new_conf[Rails.env]
+  else
+    new_settings = new_conf
   end
 
-  key = new_conf.keys.first
+  key = new_settings.keys.first
   if key
-    Rails.configuration.send("#{key}=", OpenStruct.new(new_conf[key]))
+    Rails.configuration.send("#{key}=", OpenStruct.new(new_settings[key]))
   end
 end
