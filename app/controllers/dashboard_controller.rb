0108e4815ca35ed7d655e839df638c238509e318
Phill MV <phillmv@okayfail.com>
2015-12-09 10:51:38 -0500

    API Beta users should be directed to docs, not server onboarding

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,6 +7,8 @@ class DashboardController < ApplicationController
 
     if @onboarded
       render :index
+    elsif current_user.api_beta
+      redirect_to docs_path
     else
       if params[:done]
         @opts = {notice: "Sorry, we can't see your server. Try again in a few moments, or contact us at hello@appcanary.com"}
