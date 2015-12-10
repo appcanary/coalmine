2249406dbc7e6d547ffcca4cc67b16fe3dec571c
Phill MV <phillmv@okayfail.com>
2015-12-10 09:15:08 -0500

    Fixed failing usermailer test

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
 
   def reset_password_email(user)
     @user = user
-    @link = password_reset_url(@user.reset_password_token)
+    @link = password_reset_url(:id => @user.reset_password_token)
     mail(to: user.email, :subject => "Appcanary Password Reset")
   end
 end
