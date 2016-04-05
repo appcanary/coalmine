99b2301fe7c85294bad6ab57110a2556db83d126
Max Veytsman <maxim@ontoillogical.com>
2016-04-05 12:55:50 -0400

    Blanked out the test for auth

--- test/controllers/docs_controller_test.rb
+++ test/controllers/docs_controller_test.rb
@@ -1,25 +1,16 @@
 require 'test_helper'
 
 class DocsControllerTest < ActionController::TestCase
-  describe "while unauthenticated" do
-    test "should be redirected to login" do
-      get :index
-      assert_redirected_to login_path
-    end
-  end
+  let(:user) { FactoryGirl.create(:user) }
 
-  describe "while authenticated" do
-    let(:user) { FactoryGirl.create(:user) }
-    
-    test "should render the page" do
-      User.any_instance.stubs(:agent_token).returns("1234")
+  test "should render the page" do
+    User.any_instance.stubs(:agent_token).returns("1234")
 
-      login_user(user)
-      
-      get :index
-      assert_response :success
-      assert assigns(:user)
-    end
+    login_user(user)
+
+    get :index
+    assert_response :success
+    assert assigns(:user)
   end
 
 end
