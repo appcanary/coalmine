b3b02f216f6be26b15864990e15040922658a9eb
Max Veytsman <maxim@ontoillogical.com>
2016-04-05 12:51:13 -0400

    Made docs public

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,5 +1,7 @@
 class DocsController < ApplicationController
+  skip_before_filter :require_login
+  FakeUser = Struct.new(:agent_token)
   def index
-    @user = current_user
+    @user = current_user || FakeUser.new("<YOUR_TOKEN_HERE>")
   end
 end
