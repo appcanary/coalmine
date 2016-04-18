2ef02203ed2a1ff402a474049f136b4c36fce371
Phill MV <phillmv@okayfail.com>
2016-04-18 17:21:50 -0400

    Eh, made CanaryClientTest work for now.

--- test/lib/canary_client_test.rb
+++ test/lib/canary_client_test.rb
@@ -3,19 +3,27 @@ require "#{File.dirname(__FILE__)}/../test_helper"
 class CanaryClientTest < ActiveSupport::TestCase
   setup do
     user_token = 'sn3sel6h81cdsjl7r1ea34f4pjpqbirv40sugt3mm9d9rc7it85'
-    @client = Canary.new(user_token)
+    @client = Canary2.new(user_token)
   end
 
 
   def attr_smokescreen(obj, klass)
     assert_equal klass, obj.class
 
-    params = obj.attr_params
 
     # all objects got attrs and a uuid
     assert obj.attributes.present?
     assert obj.uuid.present?
 
+    # this section of the test was *really* just testing
+    # whether AttrParams, the first version of this that I wrote,
+    # was doing the "right thing". Since it got ripped out,
+    # harder to justify this kind of smokescreen.
+    #
+    # tldr, deprecated, if its still here next time this is read,
+    # feel free to remove/replace
+=begin 
+    params = obj.attr_params
     params.each do |key|
       unless (val = obj.send(key)).nil?
         if obj.attr_enforce_collection_params.include?(key)
@@ -41,14 +49,13 @@ class CanaryClientTest < ActiveSupport::TestCase
         end
       end
     end
+=end
   end
-
-=begin
 
   describe 'status' do
     it 'returns the server status' do
       VCR.use_cassette('status') do
-        assert_equal "ok", @client.status['status']
+        assert_equal "ok", @client.get("status")['status']
       end
     end
   end
@@ -56,12 +63,12 @@ class CanaryClientTest < ActiveSupport::TestCase
   describe "vulns" do
     it "returns vulns properly wrapped" do
       VCR.use_cassette("vulnerabilities") do
-        our_vulns = @client.vulnerabilities
-        assert our_vulns.present?
-        assert our_vulns.size > 1
+        our_vulns = @client.get("vulnerabilities")
+        assert our_vulns.body.present?
+        assert our_vulns.body.size > 1
 
-        vuln = our_vulns.first
-        attr_smokescreen(vuln, Vulnerability)
+        # vuln = our_vulns.first
+        # attr_smokescreen(vuln, Vulnerability)
 
       end
     end
@@ -69,9 +76,11 @@ class CanaryClientTest < ActiveSupport::TestCase
     it "returns an indiv vuln properly wrapped" do
       VCR.use_cassette("a_vulnerability") do
         vuln_uuid = "563ba056-f6f1-4327-b744-b00c92683403"
-        vuln = @client.vulnerability(vuln_uuid)
+        vuln = @client.get("vulnerabilities/#{vuln_uuid}")
 
-        attr_smokescreen(vuln, Vulnerability)
+        assert vuln.body.present?
+
+        # attr_smokescreen(vuln, Vulnerability)
       end
     end
   end
@@ -79,14 +88,13 @@ class CanaryClientTest < ActiveSupport::TestCase
   describe "servers" do
     it "returns servers properly wrapped" do
       VCR.use_cassette("servers") do
-        servers = @client.servers
+        servers = @client.get("servers")
 
-        assert servers.present?
-        assert servers.size >= 1
+        assert servers.body.present?
+        assert servers.body.size >= 1
 
-        server = servers.first
-
-        attr_smokescreen(server, Server)
+        # server = servers.first
+        # attr_smokescreen(server, Server)
       end
     end
 
@@ -94,22 +102,11 @@ class CanaryClientTest < ActiveSupport::TestCase
       server_uuid = "5588b669-7b00-4259-a290-5bb579a86c90"
       VCR.use_cassette("a_server") do
 
-        a_server = @client.server(server_uuid)
+        a_server = @client.get("servers/#{server_uuid}")
 
-        attr_smokescreen(a_server, Server)
-      end
-    end
+        assert a_server.body.present?
 
-
-    it "returns a server's apps properly wrapped" do
-      server_uuid = "5588b669-7b00-4259-a290-5bb579a86c90"
-
-      VCR.use_cassette("a_server_apps") do
-        a_server = @client.server_apps(server_uuid)
-
-        # TODO wrap it up
-
-        # attr_smokescreen(a_server, App)
+        # attr_smokescreen(a_server, Server)
       end
     end
 
@@ -121,7 +118,7 @@ class CanaryClientTest < ActiveSupport::TestCase
       user = FactoryGirl.build :user, :email => "alicezhang@example.com"
       refute user.email.blank?
       VCR.use_cassette('create_user') do
-        result_user = @client.add_user({name: 'bob', email: user.email})
+        result_user = @client.post("users", ({name: 'bob', email: user.email}))
         assert_equal user.email, result_user['email']
       end
     end
@@ -130,11 +127,10 @@ class CanaryClientTest < ActiveSupport::TestCase
   describe 'update_user' do
     it 'should update the user email' do
       VCR.use_cassette('user_update') do
-        updated_user = @client.update_user({email: "new@example.com"})
+        updated_user = @client.put("users/me", {email: "new@example.com"})
         assert_equal 'new@example.com', updated_user['email']
       end
     end
   end
 
-=end
 end
