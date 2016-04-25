98422af54f683d44d6a734d461661a9cefceed01
Phill MV <phillmv@okayfail.com>
2016-04-25 17:29:17 -0400

    Another, faster stab at migrating

--- db/migrate/20160425184916_fix_old_is_it_vuln_results.rb
+++ db/migrate/20160425184916_fix_old_is_it_vuln_results.rb
@@ -7,9 +7,12 @@ class FixOldIsItVulnResults < ActiveRecord::Migration
         iivr.result.try(:first).try(:name)
       rescue Exception => e
         yaml = iivr.attributes_before_type_cast["result"].gsub(/!ruby.*/, "deprecated: true")
+
         new_result = YAML.load(yaml).map {|y| ArtifactVersion.parse(y) }
-        iivr.result = new_result
-        iivr.save!
+
+        new_result = YAML.dump(new_result);
+        updates = ActiveRecord::Base.send(:sanitize_sql_array, ["result = ?", new_result]);
+        IsItVulnResult.connection.update("UPDATE is_it_vuln_results SET #{updates} WHERE is_it_vuln_results.id = #{iivr.id}")
       end
     end
   end
