1da9130b4e72d0e2cc45ce2ae0a9226956845da1
Phill MV <phillmv@okayfail.com>
2016-07-05 13:57:25 -0400

    fix for failing tests on account of null constraint in bundle

--- test/factories/bundles.rb
+++ test/factories/bundles.rb
@@ -20,6 +20,7 @@ FactoryGirl.define do
   factory :bundle do
     association :account
     name { Faker::App.name }
+    platform { Platforms::Ruby }
     factory :bundle_with_packages do
       platform  { FactoryHelper.rand_platform }
       release "release"
