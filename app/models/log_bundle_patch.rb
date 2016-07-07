24703d49c1b5f5375a21c441647240ef4fc6b738
Phill MV <phillmv@okayfail.com>
2016-07-07 12:33:15 -0400

    removes code we won't be using in LBP

--- app/models/log_bundle_patch.rb
+++ app/models/log_bundle_patch.rb
@@ -26,7 +26,7 @@ class LogBundlePatch < ActiveRecord::Base
   # 2. Vulnerability that affects a package in bundle gets deleted
   # 3. Vulnerability that affects a package is edited, & package no longer affected
   
-  # untested
+  # untested; waiting for VulnMger#update
   def self.record_vulnerability_change!(vuln_id)
 
 
@@ -86,32 +86,13 @@ class LogBundlePatch < ActiveRecord::Base
             lbp.occurred_at = "bundled_package_archives".expired_at)')
 
 
-            
-=begin
-     lbv = LogBundleVulnerability.where(
-       'NOT EXISTS 
-       (SELECT 1 FROM "log_bundle_patches" lbp WHERE
-         lbp.bundle_id = "log_bundle_vulnerabilities".bundle_id AND
-         lbp.package_id = "log_bundle_vulnerabilities".package_id AND
-         lbp.bundled_package_id = "log_bundle_vulnerabilities".bundled_package_id AND
-         lbp.vulnerability_id = "log_bundle_vulnerabilities".vulnerability_id AND
-         lbp.vulnerable_package_id = "log_bundle_vulnerabilities".vulnerable_package_id)').
-       where('NOT EXISTS
-             (SELECT 1 from "bundled_packages" bp WHERE 
-               bp.bundle_id = "log_bundle_vulnerabilities".bundle_id AND
-               bp.package_id = "log_bundle_vulnerabilities".package_id AND
-               bp.id = "log_bundle_vulnerabilities".bundled_package_id)').
-       where('"log_bundle_vulnerabilities".bundle_id = ?', bundle_id)
-=end
-
-     lbv.each do |lbv|
-       self.create(:bundle_id => lbv.bundle_id,
-                   :package_id => lbv.package_id,
-                   :bundled_package_id => lbv.bundled_package_id,
-                   :vulnerability_id => lbv.vulnerability_id,
-                   :vulnerable_package_id => lbv.vulnerable_package_id,
-                   :occurred_at => lbv.occurred_at)
-     end
+    lbv.each do |lbv|
+     self.create(:bundle_id => lbv.bundle_id,
+                 :package_id => lbv.package_id,
+                 :bundled_package_id => lbv.bundled_package_id,
+                 :vulnerability_id => lbv.vulnerability_id,
+                 :vulnerable_package_id => lbv.vulnerable_package_id,
+                 :occurred_at => lbv.occurred_at)
+    end
   end
-
 end
