758b151ad80d2b0386ae7d4acc9502b46eb00121
Phill MV <phillmv@okayfail.com>
2016-07-27 11:04:45 -0400

    adds values object PlatformRelease

---
+++ app/models/platform_release.rb
@@ -0,0 +1,54 @@
+class PlatformRelease
+  class Result
+    attr_accessor :data, :error
+    def initialize(data, error = nil)
+      raise ArgumentError.new('must provide either data or error') if data.blank? && error.blank?
+      self.data = data
+      self.error = error
+    end
+
+    def to_ary
+      [data, error]
+    end
+  end
+
+
+  class Validator
+    include ActiveModel::Validations
+    validate :correct_platform_and_release
+
+    attr_accessor :platform, :release, :release_name
+    def initialize(platform, release)
+      self.platform = platform
+      self.release = release
+    end
+
+    def correct_platform_and_release
+      valid_releases = Platforms.releases_for(platform)
+
+      if valid_releases.nil?
+        errors.add(:platform, "is invalid")
+        return
+      end
+
+      if (rel_match = valid_releases[release]).blank?
+        errors.add(:release, "is invalid")
+      end
+
+      # for ubuntu, debian
+      if rel_match.is_a? String
+        self.release_name = rel_match
+      end
+    end
+  end
+
+  def self.validate(platform, release = nil)
+    pr = Validator.new(platform, release)
+
+    if pr.valid?
+      return Result.new(pr, nil)
+    else
+      return Result.new(nil, pr.errors)
+    end
+  end
+end
--- app/models/platforms.rb
+++ app/models/platforms.rb
@@ -1,8 +1,54 @@
 require File.join(Rails.root, "app/parsers", 'rpm_parser')
 class Platforms
+  Ubuntu = "ubuntu"
   Debian = "debian"
   Ruby = "ruby"
   CentOS = "centos"
+
+  RELEASES = {
+    Ruby => { nil => true },
+    CentOS => { "7" => true },
+    Ubuntu => {
+      "4.10"=>"warty",
+      "5.04"=>"hoary",
+      "5.10"=>"breezy",
+      "6.04"=>"drake",
+      "6.10"=>"edgy",
+      "7.04"=>"feisty",
+      "7.10"=>"gusty",
+      "8.04"=>"hardy",
+      "8.10"=>"intrepid",
+      "9.04"=>"jaunty",
+      "9.10"=>"karmic",
+      "10.04"=>"lucid",
+      "10.10"=>"maverick",
+      "11.04"=>"natty",
+      "12.04"=>"precise",
+      "12.10"=>"quantal",
+      "13.04"=>"raring",
+      "13.10"=>"saucy",
+      "14.04"=>"trusty",
+      "14.10"=>"utopic",
+      "15.04"=>"vivid",
+      "15.10"=>"wily",
+      "16.04"=>"xenial"
+    },
+    Debian => {
+      "2.1" => "slink",
+      "2.2" => "potato",
+      "3.0" => "woody",
+      "3.1" => "sarge",
+      "4" => "etch",
+      "5" => "lenny",
+      "6" => "squeeze",
+      "7" => "wheezy",
+      "8" => "jessie"
+    } 
+  }
+
+  def self.releases_for(platform)
+    RELEASES[platform]
+  end
 
   def self.comparator_for(package)
     klass = case package.platform
