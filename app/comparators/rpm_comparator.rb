4c684f4c92890055652cb93a5c9a5b928e3b964b
Phill MV <phillmv@okayfail.com>
2016-09-10 11:59:50 -0400

    removed el7 check from comparator: el7 support is enforced elsewhere, vulndeps now unique per release

--- app/comparators/rpm_comparator.rb
+++ app/comparators/rpm_comparator.rb
@@ -12,24 +12,11 @@ class RPMComparator
     # EVR parsers. So, let's just use the one we use elsewhere.
     constraint_evr = ::RPM::Nevra.new(version_constraint)
 
-    # ignore packages that are not el7, since we only nominally
-    # support el7 packages. Should also check arch n'est pas?
-    if constraint_evr.release =~ /el7/
-
-      (constraint_evr <=> @current_evr) <= 0
-    else
-      false
-    end
+    (constraint_evr <=> @current_evr) <= 0
   end
 
   def earlier_version?(version_constraint)
     constraint_evr = ::RPM::Nevra.new(version_constraint)
-    if constraint_evr.release =~ /el7/
-      (constraint_evr <=> @current_evr) < 0
-    else
-      false
-    end
+    (constraint_evr <=> @current_evr) < 0
   end
-
-
 end
