3ef8c5e3bce06f7c080af570c6087423e114ee6c
Phill MV <phillmv@okayfail.com>
2016-10-12 13:35:57 -0400

    changed prod to preprod

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@coalmine.appcanary.com}
-role :web, %w{deploy@coalmine.appcanary.com}
-role :db,  %w{deploy@coalmine.appcanary.com}
+role :app, %w{deploy@preprod.appcanary.com}
+role :web, %w{deploy@preprod.appcanary.com}
+role :db,  %w{deploy@preprod.appcanary.com}
 
 
 # Extended Server Syntax
