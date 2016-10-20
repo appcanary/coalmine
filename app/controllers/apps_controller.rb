7ca6d4a086b20c3ab86b776f982b0e2d55ad4da5
Phill MV <phillmv@okayfail.com>
2016-10-20 18:12:26 -0400

    minor improvements to apps#show

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -10,7 +10,7 @@ class AppsController < ApplicationController
     @server = current_user.agent_servers.find(params[:server_id])
     @bundle = current_user.bundles.where(:agent_server_id => params[:server_id]).find(params[:id])
 
-    @packages = @bundle.packages
+    @packages = @bundle.packages.order(:name)
     @vuln_packages = VulnQuery.from_bundle(@bundle)
   end
 end
