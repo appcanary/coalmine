17ed006dc72cc6c69b63f5264fa1b30e0c906bdb
Max Veytsman <maxim@ontoillogical.com>
2016-12-09 13:13:11 -0500

    Hotfix: we were showing users log resolutions for other users w/ the same packages

--- app/models/bundle.rb
+++ app/models/bundle.rb
@@ -38,7 +38,8 @@ class Bundle < ActiveRecord::Base
 
   has_many :log_bundle_vulnerabilities
   has_many :log_bundle_patches
-  has_many :log_resolutions, :through => :bundled_packages
+
+  has_many :log_resolutions, ->(bundle) { where(account_id: bundle.account_id) }, :through => :bundled_packages
 
   validates :account, presence: true
   validates :name, uniqueness: { scope: :account_id }, unless: ->(u){ u.path.present? }
