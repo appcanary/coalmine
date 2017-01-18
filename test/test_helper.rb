292fb1bf9cdf3152b55a63b6d678a97203c758ef
J Irving <j@lollyshouse.ca>
2017-01-18 11:31:03 -0500

    Try allowing connections

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -27,6 +27,7 @@ end
 VCR.configure do |c|
   c.cassette_library_dir = "test/cassettes"
   c.hook_into :webmock
+  c.allow_http_connections_when_no_cassette = true
   # c.default_cassette_options = { :record => :all }
 end
 
