ac4aa625712384ef28dd1f732d2f51d8921ed5e1
Phill MV <phillmv@okayfail.com>
2017-01-26 13:33:28 -0500

    wip onboarding improvements

--- app/controllers/onboarding_controller.rb
+++ app/controllers/onboarding_controller.rb
@@ -1,4 +1,6 @@
 class OnboardingController < ApplicationController
   def welcome
+    @agent_token = current_user ? current_user.token : "<YOUR_TOKEN_HERE>"
+    @bundle = BundlePresenter.new(VulnQuery.new(current_account), Bundle.find(726))
   end
 end
