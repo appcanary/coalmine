482d7bbb81e2c4e6c11cf6cd9a6d84bc1e3ccf6b
Phill MV <phillmv@okayfail.com>
2017-01-31 11:49:52 -0500

    split out intercom handling from init.js

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -3,30 +3,4 @@ require('jquery-ujs');
 require('bootstrap/dist/js/bootstrap.js');
 require("./internal/payment_handling.js")
 require("./internal/highlighting.js")
-
-
-$(document).ready(function() {
-
- 
-  $(".feedback").on("click", function(e) {
-    if(typeof Intercom != "undefined") {
-      e.preventDefault();
-      Intercom("showNewMessage")
-    }
-  });
-
-  $(".feedback-else").on("click", function(e) {
-    if(typeof Intercom != "undefined") {
-      e.preventDefault();
-      Intercom("showNewMessage", "Cool. Let us know in this box and we'll get in touch.")
-    }
-  });
-
-  $(".feedback-problem").on("click", function(e) {
-    if(typeof Intercom != "undefined") {
-      e.preventDefault();
-      Intercom("showNewMessage", "Sorry to hear that. Describe your problem in this box and we'll get in touch.")
-    }
-  });
-
-});
+require("./internal/handle_intercom.js")
