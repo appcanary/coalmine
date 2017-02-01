92118bc7c0db8c5fd316d942c85bbe9dca9b6486
J Irving <j@lollyshouse.ca>
2017-02-01 12:25:38 -0500

    Some user interface bits for libspector data

--- app/helpers/servers_helper.rb
+++ app/helpers/servers_helper.rb
@@ -1,2 +1,9 @@
 module ServersHelper
+  def server_processes(server_presenter)
+    if params[:debug_server_processes]
+      server_presenter.all_processes
+    else
+      server_presenter.outdated_processes
+    end
+  end
 end
