65f523762d71e83011d1da5f5c88f2c0be0e93b9
J Irving <j@lollyshouse.ca>
2017-02-24 10:32:59 -0500

    Remove select2 setup bits

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,9 +1,7 @@
 window.jQuery = window.$ = require('jquery');
 require('jquery-ujs');
-require("select2");
 require('bootstrap/dist/js/bootstrap.js');
 require("./internal/payment_handling.js")
 require("./internal/highlighting.js");
 require("./internal/handle_intercom.js");
 require("./internal/new_servers_page.js");
-require("./internal/tagging_ui.js");
--- app/assets/javascripts/internal/tagging_ui.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function () {
-  $(".tag-select").select2({tags: true});
-});
