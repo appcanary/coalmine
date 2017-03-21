7b86940a7b455090148addea423e30d9ae9fcf97
J Irving <j@lollyshouse.ca>
2017-03-21 15:37:15 -0400

    Update annotations

--- test/models/ignored_package_test.rb
+++ test/models/ignored_package_test.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: ignored_packages
+#
+#  id          :integer          not null, primary key
+#  account_id  :integer          not null
+#  user_id     :integer          not null
+#  package_id  :integer          not null
+#  bundle_id   :integer
+#  criticality :integer
+#  note        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  ignored_packages_by_account_package_bundle_ids  (account_id,package_id,bundle_id) UNIQUE
+#  index_ignored_packages_on_account_id            (account_id)
+#  index_ignored_packages_on_bundle_id             (bundle_id)
+#  index_ignored_packages_on_package_id            (package_id)
+#  index_ignored_packages_on_user_id               (user_id)
+#
+
 require 'test_helper'
 
 class IgnoredPackageTest < ActiveSupport::TestCase
