7aa800375dbb8ba8f12f06a7c98238802aaa89eb
J Irving <j@lollyshouse.ca>
2017-03-27 18:12:06 -0400

    Adds a PHP version comparator

---
+++ app/comparators/php_comparator.rb
@@ -0,0 +1,38 @@
+class PHPComparator
+  attr_accessor :version
+
+  def initialize(version_str)
+    @version = version_str
+  end
+
+  def matches?(constraints)
+    # Going to assume this means "satisfies"
+    if constraints.is_a?(Array)
+      constraint_checker.satisfies(@version, constraints)
+    elsif constraints.is_a?(String)
+      constraint_checker.satisfied_by(constraints, @version)
+    end
+  end
+
+  def vercmp(v1, v2)
+    case
+    when comparator.less_than?(v1, v2)    then -1
+    when comparator.greater_than?(v1, v2) then 1
+    when comparator.equal_to?(v1, v2)     then 0
+    end
+  end
+
+  def earlier_version?(version_constraint)
+    raise "Not implemented for PHPComparator"
+  end
+
+  private
+
+  def comparator
+    Composer::Semver::Comparator
+  end
+
+  def constraint_checker
+    Composer::Semver::Semver
+  end
+end
