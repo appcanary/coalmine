6a6cd30baaf4bc6fb2d8409bf6b94206dd3b7db6
Max Veytsman <maxim@ontoillogical.com>
2017-04-07 17:41:02 -0400

    TEMPORARY TEST

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,6 +1,8 @@
 class DocsController < ApplicationController
   skip_before_filter :require_login
   def index
+    # temporary
+    raise "SENTRY WORKS"
     @agent_token = current_user ? current_user.token : "<YOUR_TOKEN_HERE>"
   end
 end
