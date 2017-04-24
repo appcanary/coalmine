9e6749f54638c32e39ff3280affa200b7eea7843
Max Veytsman <maxim@ontoillogical.com>
2017-04-24 14:31:16 -0400

    Moved deleted_on, created_on, and delted into ArchiveBehavior

--- app/archives/agent_server_archive.rb
+++ app/archives/agent_server_archive.rb
@@ -32,25 +32,6 @@ class AgentServerArchive < ActiveRecord::Base
   extend ArchiveTableBehaviour
   belongs_to :account
 
-  scope :deleted, -> {
-    select_as_archived.
-    # look only at the most recently expired rows
-    joins("inner join (select agent_server_id, max(expired_at) expired_at from agent_server_archives group by agent_server_id) max_asa on agent_server_archives.agent_server_id = max_asa.agent_server_id and agent_server_archives.expired_at = max_asa.expired_at").
-    # look only at rows that do not currently exist in AgentServer table
-    joins("left join agent_servers on agent_servers.id = agent_server_archives.agent_server_id").
-    where("agent_servers.id is null").
-    order("id, agent_server_archives.expired_at DESC")
-
-  }
-
-     
-  # note for later:
-  # select agent_server_archives.* from agent_server_archives inner join (select agent_server_id, max(id) id from agent_server_archives WHERE (valid_at >= '2017-01-12 00:00:00.000000' and valid_at <= '2017-01-12 23:59:59.999999') group by agent_server_id) specific_as 
-  # ON specific_as.id = agent_server_archives.id 
-  # left join agent_servers on agent_server_archives.agent_server_id = agent_servers.id
-  # where agent_servers.id is null and agent_server_archives.account_id = 22
-  # order by agent_server_archives.agent_server_id
-
   #TODO: this should be in the presenter
   def display_name
     name.blank? ? hostname : name
--- app/models/archive_behaviour.rb
+++ app/models/archive_behaviour.rb
@@ -1,4 +1,4 @@
-# TODO: document
+# TODO: document and TEST!
 module ArchiveBehaviour
   def self.extended(klass)
     archive_class = (klass.to_s + "Archive").constantize
@@ -17,6 +17,31 @@ module ArchiveBehaviour
     union_str(all.where("valid_at <= ? and expired_at > ?", time_t, time_t),
               self::ARCHIVE_CLASS.select_as_archived.where("valid_at <= ? and expired_at > ?", time_t, time_t))
 
+  end
+
+  def deleted
+    archive_table_name = self::ARCHIVE_CLASS.table_name
+    archived_col = (self::ARCHIVE_CLASS)::ARCHIVED_COL
+
+    self::ARCHIVE_CLASS.select_as_archived.
+      # look only at the most recently expired rows
+      joins("inner join (select #{archived_col}, max(expired_at) expired_at from #{archive_table_name} group by #{archived_col}) max_row on #{archive_table_name}.#{archived_col} = max_row.#{archived_col} and #{archive_table_name}.expired_at = max_row.expired_at").
+      # look only at rows that do not currently exist in AgentServer table
+      joins("left join #{self.table_name} on #{self.table_name}.id = #{archive_table_name}.#{archived_col}").
+      where("#{self.table_name}.id is null").
+      order("id, #{archive_table_name}.expired_at DESC")
+  end
+
+
+  def created_on(date)
+    from(union_str(all, self.deleted)).
+      where('created_at >= ? and created_at <= ?', date.at_beginning_of_day, date.at_end_of_day)
+  end
+
+  def deleted_on(date)
+    from("(#{self.deleted.to_sql}) #{self.table_name}").
+      # and only look at stuff from this day in particular
+      where("expired_at >= ? and expired_at <= ?", date.at_beginning_of_day, date.at_end_of_day)
   end
 
   def revisions
