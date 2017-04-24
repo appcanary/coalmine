c34f0ac7a705426ec8bf9166bb6a619d0785e693
Max Veytsman <maxim@ontoillogical.com>
2017-04-24 13:33:10 -0400

    Refactored the daily summary presenter

--- app/presenters/daily_summary_presenter.rb
+++ app/presenters/daily_summary_presenter.rb
@@ -1,18 +1,20 @@
 class DailySummaryPresenter
-  attr_accessor :date, :account, :vulnquery, :fresh_vulns, :new_vulns, :patched_vulns, :cantfix_vulns, :changes, :server_ct, :inactive_server_ct, :new_servers, :deleted_servers
+  attr_accessor :date, :account, :vulnquery,
+                :fresh_vulns, :new_vulns, :patched_vulns, :cantfix_vulns,
+                :changes, :server_ct, :inactive_server_ct, :new_servers, :deleted_servers
 
   def initialize(query)
     @account = query.account
     @vulnquery = VulnQuery.new(query.account)
     @date = query.date
 
-    @fresh_vulns = FreshVulnsPresenter.new(query.fresh_vulns)
-    @new_vulns = NewVulnsPresenter.new(query.new_vulns)
+    @fresh_vulns = VulnCollectionPresenter.new(query.fresh_vulns)
+    @new_vulns = VulnCollectionPresenter.new(query.new_vulns)
 
     @all_vuln_ct = query.all_vuln_ct
 
-    @patched_vulns = PatchedVulnsPresenter.new(query.patched_vulns)
-    @cantfix_vulns = CantFixVulnsPresenter.new(query.cantfix_vulns)
+    @patched_vulns = VulnCollectionPresenter.new(query.patched_vulns)
+    @cantfix_vulns = VulnCollectionPresenter.new(query.cantfix_vulns)
     @changes = ChangesPresenter.new(query.changes)
 
     @all_servers = query.all_servers
@@ -23,112 +25,6 @@ class DailySummaryPresenter
     active_server_ct = @all_servers.active_as_of(@date).count
     @inactive_server_ct = @server_ct - active_server_ct
   end
-
-  module SortVulnsByCritAndPackages
-    def sort_group_log_vulns(query)
-      query.group_by(&:vulnerability).
-        reduce({}) { |hsh, (vuln, logs)|  
-        hsh[vuln] = logs.uniq(&:package_id).map(&:package); 
-        hsh
-
-      }.sort_by { |vuln, pkgs| 
-        [-vuln.criticality_ordinal, -pkgs.size] 
-      }
-    end
-  end
-
-  class FreshVulnsPresenter
-    include SortVulnsByCritAndPackages
-    attr_accessor :vuln_ct, :package_ct, :server_ct, :sorted_vulns, :package_ids, :server_ids
-
-    delegate :each, to: :sorted_vulns
-
-    def initialize(fresh_vulns)
-      @sorted_vulns = sort_group_log_vulns(fresh_vulns)
-
-      @vuln_ct = fresh_vulns.map(&:vulnerability_id).uniq.size
-      @package_ct = fresh_vulns.map(&:package_id).uniq.size
-      @server_ct = fresh_vulns.map(&:agent_server_id).uniq.size
-      @server_ids = fresh_vulns.map(&:agent_server_id).uniq
-      @package_ids = fresh_vulns.map(&:package_id).uniq
-    end
-  end
-
-  class NewVulnsPresenter
-    include SortVulnsByCritAndPackages
-    attr_accessor :vuln_ct, :package_ct, :server_ct, :supplementary_ct, :sorted_vulns, :package_ids, :server_ids
-
-    delegate :each, to: :sorted_vulns
-
-    def initialize(new_vulns)
-      @sorted_vulns = sort_group_log_vulns(new_vulns)
-
-      @vuln_ct = new_vulns.map(&:vulnerability_id).uniq.size
-      @package_ct = new_vulns.map(&:package_id).uniq.size
-
-      @server_ct = new_vulns.map(&:agent_server_id).uniq.size
-      @server_ids = new_vulns.map(&:agent_server_id).uniq
-      @package_ids = new_vulns.map(&:package_id).uniq
-
-      new_supplmenetary_vulns = new_vulns.select(&:supplementary)
-      @supplmenetary_ct = new_supplmenetary_vulns.map(&:vulnerability_id).uniq.size
-    end
-
-  end
-
-  class PatchedVulnsPresenter
-    include SortVulnsByCritAndPackages
-    attr_accessor :vuln_ct, :package_ct, :server_ct, :supplementary_ct, :sorted_vulns
-
-    delegate :each, to: :sorted_vulns
-
-    def initialize(patched_vulns)
-      @sorted_vulns = sort_group_log_vulns(patched_vulns)
-
-      @vuln_ct = patched_vulns.map(&:vulnerability_id).uniq.size
-      @package_ct = patched_vulns.map(&:package_id).uniq.size
-      @server_ct = patched_vulns.map(&:agent_server_id).uniq.size
-      @supplementary_ct = patched_vulns.select(&:supplementary).map(&:vulnerability_id).uniq.size
-    end
-
-    def has_supplementary?
-      supplementary_ct > 0
-    end
-  end
-
-  class CantFixVulnsPresenter
-    include SortVulnsByCritAndPackages
-    attr_accessor :vuln_ct, :package_ct, :server_ct, :supplementary_ct, :sorted_vulns
-
-    delegate :each, to: :sorted_vulns
-
-    def initialize(cantfix_vulns)
-      @sorted_vulns = sort_group_log_vulns(cantfix_vulns)
-
-      @vuln_ct = cantfix_vulns.map(&:vulnerability_id).uniq.size
-      @package_ct = cantfix_vulns.map(&:package_id).uniq.size
-      @server_ct = cantfix_vulns.map(&:agent_server_id).uniq.size
-      @supplementary_ct = cantfix_vulns.select(&:supplementary).map(&:vulnerability_id).uniq.size
-    end
-
-    def has_supplementary?
-      supplementary_ct > 0
-    end
-  end
-
-
-
-  class ChangesPresenter
-    attr_accessor :added_ct, :removed_ct, :upgraded_ct, :server_ct
-
-    def initialize(new_changes)
-      @server_ct = new_changes[:server_ct]
-      @added_ct = new_changes[:added_ct]
-      @removed_ct = new_changes[:removed_ct]
-      @upgraded_ct = new_changes[:upgraded_ct]
-    end
-  end
-
 
   def total_vuln_ct
     fresh_vulns.vuln_ct + new_vulns.vuln_ct
@@ -146,35 +42,6 @@ class DailySummaryPresenter
     (fresh_vulns.server_ids + new_vulns.server_ids).uniq.size
   end
 
-
-  def has_fresh_vulns?
-    fresh_vulns.vuln_ct > 0
-  end
-
-  def has_new_vulns?
-    new_vulns.vuln_ct > 0
-  end
-
-  def has_patched_vulns?
-    patched_vulns.vuln_ct > 0
-  end
-
-  def has_cantfix_vulns?
-    cantfix_vulns.vuln_ct > 0
-  end
-
-  def has_changes?
-    changes.server_ct > 0
-  end
-
-  def has_new_servers?
-    new_servers.any?
-  end
-
-  def has_deleted_servers?
-    deleted_servers.any?
-  end
-
   def anything_to_report?
     has_vulns_or_servers_to_report? || has_changes_to_report?
   end
@@ -189,7 +56,7 @@ class DailySummaryPresenter
 
   def has_patchable_vulns_to_report?
     has_fresh_vulns? ||
-      has_new_vulns? 
+      has_new_vulns?
   end
 
   def has_vulns_to_report?
@@ -225,4 +92,69 @@ class DailySummaryPresenter
     end
   end
 
+  def self.create_collection_has_any_methods(*colls)
+    colls.each do |coll_name|
+      define_method :"has_#{coll_name}?" do
+        coll = self.send(coll_name)
+        coll.any?
+      end
+    end
+  end
+
+  create_collection_has_any_methods :fresh_vulns, :new_vulns, :patched_vulns, :cantfix_vulns, :changes, :new_servers, :deleted_servers
+
+
+
+  module SortVulnsByCritAndPackages
+    def sort_group_log_vulns(query)
+      query.group_by(&:vulnerability).
+        reduce({}) { |hsh, (vuln, logs)|
+        hsh[vuln] = logs.uniq(&:package_id).map(&:package);
+        hsh
+
+      }.sort_by { |vuln, pkgs|
+        [-vuln.criticality_ordinal, -pkgs.size]
+      }
+    end
+  end
+
+  class VulnCollectionPresenter
+    include SortVulnsByCritAndPackages
+    attr_accessor :vuln_ct, :package_ct, :server_ct, :sorted_vulns, :package_ids, :server_ids, :supplementary_ct
+
+    delegate :each, to: :sorted_vulns
+    def initialize(coll)
+      self.sorted_vulns = sort_group_log_vulns(coll)
+
+      self.vuln_ct = coll.map(&:vulnerability_id).uniq.size
+      self.package_ct = coll.map(&:package_id).uniq.size
+      self.server_ct = coll.map(&:agent_server_id).uniq.size
+      self.server_ids = coll.map(&:agent_server_id).uniq
+      self.package_ids = coll.map(&:package_id).uniq
+      self.supplementary_ct = coll.select(&:supplementary).map(&:vulnerability_id).uniq.size
+    end
+
+    def has_supplementary?
+      self.supplementary_ct > 0
+    end
+
+    def any?
+      self.vuln_ct > 0
+    end
+  end
+
+  class ChangesPresenter
+    attr_accessor :added_ct, :removed_ct, :upgraded_ct, :server_ct
+
+    def initialize(new_changes)
+      self.server_ct = new_changes[:server_ct]
+      self.added_ct = new_changes[:added_ct]
+      self.removed_ct = new_changes[:removed_ct]
+      self.upgraded_ct = new_changes[:upgraded_ct]
+    end
+
+    def any?
+      self.server_ct > 0
+    end
+  end
 end
