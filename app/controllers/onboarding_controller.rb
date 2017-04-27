14703a641c05a3c7f5a1528116697906b6fab0f5
Phill MV <phillmv@okayfail.com>
2017-04-27 17:51:36 -0400

    d'oh

--- app/controllers/onboarding_controller.rb
+++ app/controllers/onboarding_controller.rb
@@ -1,6 +1,5 @@
 class OnboardingController < ApplicationController
   def welcome
     @agent_token = current_user ? current_user.token : "<YOUR_TOKEN_HERE>"
-    @bundle = BundlePresenter.new(VulnQuery.new(current_account), Bundle.find(726))
   end
 end
