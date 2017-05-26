a6f402c327424561bd6cbe141c4b1fba1cc72230
Max Veytsman <maxim@ontoillogical.com>
2017-05-26 13:17:16 -0400

    Dead code

--- app/controllers/cves_controller.rb
+++ app/controllers/cves_controller.rb
@@ -1,10 +1,6 @@
 class CvesController < ApplicationController
   skip_before_filter :require_login
 
-  def index
-    @cves = Advisory.from_cve.all
-  end
-
   def show
     @cve = Advisory.from_cve.find_by!(:identifier => params[:cve_id])
     @vulns = Vulnerability.by_cve_id(params[:cve_id])
