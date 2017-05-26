2eacaaca51e7a2dfa40b122e132a2197fa65e176
Max Veytsman <maxim@ontoillogical.com>
2017-05-26 00:49:24 -0400

    Hotfix: Split out Alpine CVEs and XSAs for xen vulns, add icon

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -48,6 +48,8 @@ module DashboardHelper
                     ["debian.png", "Debian"]
                   when "amzn"
                     ["amzn.png", "Amazon Linux"]
+                  when "alpine"
+                    ["alipne.png", "Alpine Linux"]
                   end
 
     content_tag(:span) do 
